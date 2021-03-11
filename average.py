5e9062a24a699811427900a712a4c89bed3f71d9
iillyy <ma_natsu03@naver.com>
2021-03-12 01:58:09 +0900

    수정

--- average.py
+++ average.py
@@ -1,10 +1,10 @@
 case = int(input())
 
-for i in range(0,case):
+for i in range(case):
     score = list(map(int,input().split()))
     average = sum(score[1:])/score[0]
+    count = 0
     for j in score[1:]:
-        count = 0
         if j > average:
             count += 1
     over= count / score[0] * 100
