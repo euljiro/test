6d4b9c6763e4a8c29f7e676ef0b6b1b906ed4754
iillyy <ma_natsu03@naver.com>
2021-03-11 21:36:51 +0900

    def basic

---
+++ def.py
@@ -0,0 +1,10 @@
+def solve(a):     #a라는 변수를 받는 solve라는 함수
+    return sum(a)  #내장함수 사용
+
+
+def solve(a):  #a라는 변수를 받는 solve라는 함수
+    total = 0    #시작
+    for i in a: #a라는 리스트는 돕니다
+        total += a #토탈은 a값을 계속 더한 값이에요
+    return total
+    
