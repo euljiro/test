af4e1dd93af12a57e418529cd64a0add8ae5ded5
iillyy <ma_natsu03@naver.com>
2021-03-11 16:38:46 +0900

    eof

--- while.py
+++ while.py
@@ -1,7 +1,17 @@
-while True:
-    a,b=list(map(int,input().split()))
+# while True:
+#     a,b=list(map(int,input().split()))
     
-    if a==0 and b==0:
-        break
-    else:
+#     if a==0 and b==0:
+#         break
+#     else:
+#         print(a+b)
+
+
+# EOF(end of file)
+while True:
+    try:
+        a,b=list(map(int,input().split()))
         print(a+b)
+    
+    except:
+        break
