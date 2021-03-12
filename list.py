c3886cbda44421fba4daf824e35c927dc57af793
iillyy <ma_natsu03@naver.com>
2021-03-12 23:52:48 +0900

    number sort

--- list.py
+++ list.py
@@ -1,23 +1,23 @@
-# case = int(input())
-# location = []
+case = int(input())
+location = []
 
-# for i in range(0,case):
-#     num = int(input())
-#     location.append(num)
+for i in range(0,case):
+    num = int(input())
+    location.append(num)
 
 
-# for i in range(len(location)):
-#     j=i
-#     index= location[i]
-#     while j > 0 and location[j-1] > index:
-#         location[j] = location[j-1]
-#         j-=1
-#     location[j] = index
+for i in range(len(location)):
+    j=i
+    index= location[i]
+    while j > 0 and location[j-1] > index:
+        location[j] = location[j-1]
+        j-=1
+    location[j] = index
 
-# print(location)
+print(location)
 
-# case = int(input())
-# location = []
+case = int(input())
+location = []
 
 
 case = int(input())
