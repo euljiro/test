e8bdfb34eaf8bd1aa453e2fd3feeb74467b5c215
iillyy <ma_natsu03@naver.com>
2021-03-15 19:41:14 +0900

    cover

--- dfsbfs.py
+++ dfsbfs.py
@@ -1,34 +1,30 @@
-from collections import deque
-
 def dfs(v):
-    visited[v] = True
-    print(v, end='')
-    for i in range(1, n+1):
-        if visited[i] == 0 and graph[v][i] ==1 :
+    print(v, end=' ')
+    visit[v] = 1
+    for i in range(1, n + 1):
+        if visit[i] == 0 and s[v][i] == 1:
             dfs(i)
 
 def bfs(v):
-    queue = deque([v])
-    visited[v] = 0
-    while queue:
-        v = queue.popleft()
-        print(v, end='')
-        for i in range(1, n+1):
-            if visited[i]==1 and graph[v][i]==1:
+    queue = [v]
+    visit[v] = 0
+    while(queue):
+        v = queue[0]
+        print(v, end=' ')
+        del queue[0]
+        for i in range(1, n + 1):
+            if visit[i] == 1 and s[v][i] == 1:
                 queue.append(i)
-                visited[i] = 0
-
+                visit[i] = 0
 
 n, m, v = map(int, input().split())
-graph = [[0]*(n+1) for i in range(n+1)]
-visited=[0 for i in range(n+1)]
-
+s = [[0] * (n + 1) for i in range(n + 1)]
+visit = [0 for i in range(n + 1)]
 for i in range(m):
     x, y = map(int, input().split())
-    graph[x][y] = 1
-    graph[y][x] = 1
-
+    s[x][y] = 1
+    s[y][x] = 1
+    
 dfs(v)
 print()
 bfs(v)
-
