af9dfe898098aaaf7338fdd41e16ee4ec23d3014
iillyy <ma_natsu03@naver.com>
2021-03-19 10:45:18 +0900

    nm

--- nm.py
+++ nm.py
@@ -1,3 +1,29 @@
+# n, m = map(int, input().split())
+# check = [False]*n
+# ans = [0]*m
+
+
+# def nm(idx):
+          
+#     if idx == m:
+#         print(*ans)
+#         return
+
+#     for i in range(n):
+#         if check[i]:
+#             continue
+#         else:
+#             check[i] =True
+#             ans[idx] = i+1
+
+#         nm(idx+1)
+
+#         for j in range(i+1, n):
+#             check[j] = False
+
+# nm(0)
+
+
 n, m = map(int, input().split())
 check = [False]*n
 ans = [0]*m
@@ -18,8 +44,8 @@ def nm(idx):
 
         nm(idx+1)
 
-        for j in range(i+1, n):
-            check[j] = False
+
+        check[i] = False
 
 nm(0)
 
